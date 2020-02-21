77edab22febc192d9477026a4b4f1783dd2da7b3
Andhy Gomez <47075449+AndhyGomez@users.noreply.github.com>
2020-02-21 13:21:48 -0500

    Uploading Initial Files
    Contains AIGame, Sudoku, and LoShu

---
+++ src/LoShu.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class LoShu extends AIGame
+{
+
+	public LoShu(String filePath) throws FileNotFoundException, IOException 
+	{
+		super(filePath);
+	
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
